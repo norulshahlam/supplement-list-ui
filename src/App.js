3a847e87820841c9898dcee5dbafdbc98613316b
norulshahlam <norulshahlam@gmail.com>
2022-11-23 23:32:58 +0800

    q

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import Login from "./components/Login";
 import Navbar from "./components/Navbar";
 import TailwindSupplementList from "./components/TailwindSupplementList";
 import { useState } from "react";
-import ReactLogger from "react-terminal-logger/console-logger";
 
 function App() {
-  ReactLogger.start();
 
   const [login, setLogin] = useState(false);
   return (
