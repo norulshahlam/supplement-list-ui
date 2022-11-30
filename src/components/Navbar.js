1e807aeba9eee739516fa16a8e7ac6298eac647f
norulshahlam <norulshahlam@gmail.com>
2022-11-30 12:16:16 +0800

    change deployment hostname

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,13 +17,13 @@ const Navbar = ({setLogin}) => {
       
       <button
         onClick={exportCsv}
-        className="px-2 py-1 mx-2 text-gray bg-white rounded-md hover:text-white hover:bg-gray-800 hover:cursor-pointer"
+        className="px-2 py-1 my-1 mx-2 text-gray bg-white rounded-md hover:text-white hover:bg-gray-800 hover:cursor-pointer"
       >
        Export
       </button>
       <button
         onClick={()=>setLogin(true)}
-        className="px-4 py-2 mx-2 text-gray bg-white rounded-md hover:text-white hover:bg-gray-800 hover:cursor-pointer"
+        className="px-4 py-2 my-1 mx-2 text-gray bg-white rounded-md hover:text-white hover:bg-gray-800 hover:cursor-pointer"
       >
         <AiOutlineLogin/>
       </button>
